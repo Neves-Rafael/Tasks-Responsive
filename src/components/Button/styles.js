7aaef1708dff188ef69e2d9501c1b503136fb8b4
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-29 15:10:31 -0300

    :lipstick: Style adjust button and input component

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,25 +1,25 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.button`
   background-color: ${({ theme }) => theme.COLORS.BLUE_300};
   color: ${({ theme }) => theme.COLORS.BLUE_600};
-  
+
   border: 0;
-  height: 56px;
-  padding: 0 16px;
+  height: 3.5rem;
+  padding: 0 1rem;
   font-weight: 500;
-  border-radius: 7px;
-  
+  border-radius: 0.43rem;
+
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 5px;
 
   > span {
-    font-size: 16px;
+    font-size: 1rem;
   }
 
   > svg {
-    font-size: 22px;
+    font-size: 1.37;
   }
 `;
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -4,18 +4,18 @@ export const Container = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
-  
+
   background-color: ${({ theme }) => theme.COLORS.GRAY_700};
   color: ${({ theme }) => theme.COLORS.GRAY_100};
-  
-  border-radius: 7px;
-  
+
+  border-radius: 0.43rem;
+
   > input {
-    height: 56px;
+    height: 3.5rem;
     width: 100%;
-    font-size: 16px;
-    padding: 12px;
-    
+    font-size: 1rem;
+    padding: 0.75rem;
+
     color: ${({ theme }) => theme.COLORS.GRAY_100};
     background: transparent;
     border: 0;
@@ -24,10 +24,10 @@ export const Container = styled.div`
       color: ${({ theme }) => theme.COLORS.GRAY_300};
     }
   }
-  
+
   > svg {
-    margin-left: 16px;
-    font-size: 24px;
+    margin-left: 1rem;
+    font-size: 1.5rem;
     color: ${({ theme }) => theme.COLORS.GRAY_400};
   }
 `;
